1fd8abcb66a9cb1113a3d859a2298f4ddd4197ec
Gerti23 <gertibajo17@gmail.com>
2024-11-22 13:39:54 +0000

    update

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,14 +1,13 @@
 function handleResponseFromAPI(promise) {
-    return promise
-      .then(() => ({
-        status: 200,
-        body: 'success',
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
       }))
       .catch(() => new Error())
-      .finally(() => {
-        console.log('Got a response from the API');
-      });
-  }
-  
-  export default handleResponseFromAPI;
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}  
+export default handleResponseFromAPI;
   
