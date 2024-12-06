a8e6948d0c1ea9de1c5e487ed276476d4c24838d
Gerti23 <gertibajo17@gmail.com>
2024-12-06 17:29:12 +0000

    Finish

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """Tasks"""
 
 from typing import List
@@ -9,3 +10,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of delays"""
     delays = [task_wait_random(max_delay) for _ in range(n)]
     return [await delay for delay in delays]
+    
